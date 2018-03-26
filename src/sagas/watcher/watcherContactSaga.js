a1f4fb5b9e770db286853a81a6b7e04950c49764
marcospagnulo <marco.spagnulo@gmail.com>
2018-03-26 10:37:37 +0200

    removed unused action

--- src/sagas/watcher/watcherContactSaga.js
+++ src/sagas/watcher/watcherContactSaga.js
@@ -1,23 +1,6 @@
 import { takeLatest } from 'redux-saga/effects';
 import * as contactSaga from '../service/serviceContactSaga';
-import * as profileSaga from '../service/serviceProfileSaga';
-import * as messageSaga from '../service/serviceMessageSaga';
 import * as types from '../../store/actionTypes';
-
-// LOGIN watcher
-export function* watchLogin() {
-    yield takeLatest(types.LOGIN, profileSaga.loginSaga);
-}
-
-// GET PROFILE watcher
-export function* watchGetProfile() {
-    yield takeLatest(types.GET_PROFILE, profileSaga.getProfileSaga);
-}
-
-// UPDATE PROFILE watcher
-export function* watchSubmitProfile() {
-    yield takeLatest(types.SUBMIT_PROFILE, profileSaga.submitProfileSaga);
-}
 
 // SAVE CONTACT watcher
 export function* watchSubmitContact() {
@@ -32,23 +15,4 @@ export function* watchGetContacts() {
 // DELETE CONTACTS watcher
 export function* watchDeleteContacts() {
     yield takeLatest(types.DELETE_CONTACT, contactSaga.deleteContactSaga);
-}
-
-// SAVE MESSAGE watcher
-export function* watchSendMessage() {
-    yield takeLatest(types.SEND_MESSAGE, messageSaga.sendMessageSaga);
-}
-
-export function* watchReadMessage() {
-    yield takeLatest(types.READ_MESSAGE, messageSaga.readMessageSaga);
-}
-
-// GET MESSAGE watcher
-export function* watchGetMessages() {
-    yield takeLatest(types.GET_MESSAGES, messageSaga.getMessagesSaga);
-}
-
-// DELETE MESSAGE watcher
-export function* watchDeleteMessage() {
-    yield takeLatest(types.DELETE_MESSAGE, messageSaga.deleteMessageSaga);
 }
